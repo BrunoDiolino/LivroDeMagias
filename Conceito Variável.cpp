f353c092b9eb038e63927b59f0cface8bccf168f
Bruno Diolino <brunoemanuel1pereira@gmail.com>
2024-12-25 21:58:13 -0300

    Add files via upload

---
+++ Conceito Variável.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+#include <tchar.h>
+using namespace std;
+
+int main (){
+	   _tsetlocale(LC_ALL, _T("portuguese")); //função que cofigura o console windows para mostrar acentos 
+	   //declaração de variaveis  
+	   //prmieiro colocamos qual tipo de variavel queremos que o computador crie
+	   //ao executar o programa
+	   //neste caso escolhemos ambas como int ( integer)
+	   // ou seja o local da memoria que estamos solicitando ao computador 
+	   //deve ser capaz de armazenar numeros inteiros 
+	   int numvidas = 5;
+		int score = 1350;
+		cout << " ************ INICIO DO JOGO ***************** " << "/n";
+		cout << "Vidas Jogador: " << numvidas << "/n";
+		cout << "Score: " << score << "/n";
+		cout << "Espaço que numvidas ocupa na memória Ram< " << &numvidas << "/n"; //& mostra o espaço em que está alocado a variavel e as informações
+		cout << "Espaço que score ocupa na memória Ram< " << &score << "/n";
+
+		cout << " ******************************************** " << "/n";
+	
+		cout << " ************ DURENTE O JOGO **************** " << "/n";
+		score = score + 150; // score += 150;
+		numvidas = numvidas - 1; // numvidas -= 1;
+		cout << "Vidas Jogador: " << numvidas << "/n";
+		cout << "Score: " << score << "/n";
+		cout << " ******************************************** " << "/n";
+
+
+		
+		system("PAUSE"); 
+		
+
+
+
+
+
+	return 0;
+
+}
---
+++ Float&Double.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+
+	int nmr1;
+	float nmr2 ;
+	double nmr3 = 45350.904555;
+	
+	nmr1 = 45;
+	nmr2 = 55.56f;
+
+		cout << "Valor do Numero: " << nmr1 << "\n";
+		cout << "tamanho da vriavel Numero" << sizeof(nmr1) << "Bytes" << "\n";
+		cout << "endereço carregado na memória" << &nmr1 << "\n";
+
+		cout << "Valor do Numero: " << nmr2 << "\n";
+		cout << "tamanho da vriavel Numero" << sizeof(nmr2) << "Bytes" << "\n";
+		cout << "endereço carregado na memória" << &nmr2 << "\n";
+
+		cout << "Valor do Numero: " << setprecision(12) << nmr3 << "\n"; // setprecision vai mostrar o double de formar correta
+		cout << "tamanho da vriavel Numero" << sizeof(nmr3) << "Bytes" << "\n";
+		cout << "endereço carregado na memória" << &nmr3 << "\n";
+
+
+		system("PAUSE");
+
+
+	return 0;
+}
