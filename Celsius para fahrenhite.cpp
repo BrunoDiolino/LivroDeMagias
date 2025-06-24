993f92647a1ccd65ecd3f6818b69d622d3d5827f
Bruno Diolino <brunoemanuel1pereira@gmail.com>
2025-06-24 16:55:12 -0300

    Add files via upload

---
+++ Celsius para fahrenhite.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+ 
+using namespace std;
+
+int main(){
+
+    int temp;
+    float fah,res;
+
+    cout<<"qual a temperatura desejada para a conversao :"<<endl;
+    cin>>temp;
+    fah=32;
+
+    res==temp*9/5+32;
+
+    cout<<res<<"°F"<<endl;
+
+    return 0;
+}
