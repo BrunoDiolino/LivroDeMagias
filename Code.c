72c5b350f6a9c09df7f465ad57ab494f3cdd1aee
Bruno Diolino <brunoemanuel1pereira@gmail.com>
2025-12-11 22:54:09 -0300

    Add files via upload

---
+++ Code.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {    
+    printf("*********************************\n");
+    printf("Bem-vindo ao jogo da adivinhação!\n");
+    printf("*********************************\n");
+
+}
