6ed9ddee1b66b14836e53aef9b60e61b844634ce
Bruno Diolino <brunoemanuel1pereira@gmail.com>
2026-03-26 16:18:07 -0300

    Implement guessing game logic in Jogo_da_adv.c
    Fix loop initialization and add game logic for guessing.

---
+++ Jogo_da_adv.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {    
+    printf("*********************************\n");
+    printf("Bem-vindo ao jogo da adivinhação!\n");
+    printf("*********************************\n");
+
+    int Numsec = 42;
+    int chute;
+
+    for(int=i;i<=3;i++){
+        printf("Tentativa %d de 3\n",i);
+        printf("qual é o seu chute? ");
+
+        int acertou = (chute == Numsec);
+
+        if(acertou){
+            printf("paraben!!Você acertou!\n");
+            printf("Jogue de novo, você é um bom jogador\n");
+        } 
+        else{
+        int maior = chute > Numsec
+        }
+    }
+}
