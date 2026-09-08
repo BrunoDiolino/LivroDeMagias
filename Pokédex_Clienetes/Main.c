3b917ba3a5d437c9418a7c5aab6131e17764ddda
Bruno Diolino <brunoemanuel1pereira@gmail.com>
2026-09-08 16:38:20 -0300

    Add files via upload

---
+++ Pokédex_Clienetes/Main.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+
+int main(){
+ char empresa[20], Info[250], ID[10];
+ int Valor, Dd,Mm,Yy,Nav = 1,Adc = 2,Edit = 3,Close = 4;
+
+  printf("  ===Busyness Maneger===   \n");
+  printf("MENU:                      \n");
+  printf("1.Navegar\n");
+  sscanf(Nav, "%d");
+  printf("2.Adicoinar\n");
+  sscanf(Adc, "%d");
+  printf("3.Editar\n");
+  sscanf(Edit, "%d");
+  printf("4.Encerrar\n");
+  sscanf(Close, "%d");    
+  return break;
+  printf("____________/__/___________\n");
+
+  if(Nav){}
+
+
+
+  return 0
+}
